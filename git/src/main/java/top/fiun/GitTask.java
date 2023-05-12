a854e2b1d3a7d9d838d583e78ad354094814f010
Wang Encheng <wangencheng0111@outlook.com>
2023-05-12 13:34:33 +0800

    Attempt to enable feature "git pull".

--- git/src/main/java/top/fiun/GitTask.java
+++ git/src/main/java/top/fiun/GitTask.java
@@ -5,11 +5,14 @@ import org.eclipse.jgit.api.Git;
 
 import org.eclipse.jgit.api.PullCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
 import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GHIssueBuilder;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
@@ -20,10 +23,13 @@ public class GitTask {
     private GHRepository originalRepository;
     private GHRepository ownRepository;
     private Git localWorkplace;
+    private File localStorage;
 
-    public GitTask(GitHub gitHubConnection, Account account, URI originalURI) {
+    public GitTask(GitHub gitHubConnection, Account account, URI originalURI, File localStoragePath) throws IOException {
         this.gitHubConnection = gitHubConnection;
         this.account = account;
+        this.localStorage = new File(localStoragePath.getAbsoluteFile()+"/.git");
+        this.localWorkplace = new Git(new FileRepository(this.localStorage));
     }
 
     public void commitLocalChange(String message) throws GitAPIException {
@@ -34,13 +40,15 @@ public class GitTask {
     }
 
     public void pullFromOriginalRepository() throws IOException {
+        // fork
         this.ownRepository = this.originalRepository.fork();
-
         // pull
+        localWorkplace.pull().setRemote(ownRepository.getHtmlUrl().toString());
     }
 
     public void pushToOriginalRepository() {
         // push
+
         // commit PR
     }
 
@@ -52,5 +60,9 @@ public class GitTask {
         GHIssueBuilder issueBuilder = this.originalRepository.createIssue(title);
         issueBuilder.assignee(gitHubConnection.getUser(gitHubConnection.getMyself().getLogin()));
         issueBuilder.body(message);
+    }
+
+    private void downLoadRemoteRepository(URI remoteRepositoryURI) {
+
     }
 }
